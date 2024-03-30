def7ab09a36db9c1e6eb82016b74b2d785166530
xbt <xavier.basset@hoomano.com>
2024-03-30 12:06:59 +0100

    clean code

--- mojodex_core/db.py
+++ mojodex_core/db.py
@@ -2,15 +2,18 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
+import logging
 
 from mojodex_core.entities import MdError
 
-Base = declarative_base()
+try:
 
-connection_string = f"postgresql+psycopg2://{os.environ['DBUSER']}:{os.environ['DBPASS']}@{os.environ['DBHOST']}:5432/{os.environ['DBNAME']}"
+    Base = declarative_base()
 
-engine = create_engine(connection_string)
-Base.metadata.create_all(engine)
-db_session = Session(engine)
+    connection_string = f"postgresql+psycopg2://{os.environ['DBUSER']}:{os.environ['DBPASS']}@{os.environ['DBHOST']}:5432/{os.environ['DBNAME']}"
 
-# print(db_session.query(MdError).first())
+    engine = create_engine(connection_string)
+    Base.metadata.create_all(engine)
+    db_session = Session(engine)
+except Exception as e:
+    logging.warning(f"Error initializing db_session :: {e}")

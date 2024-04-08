683909fa7ad10b658005741dc732d403c55348f1
KellyRoussel <kelly.roussel@hoomano.com>
2024-04-08 15:32:27 +0200

    reset correct workflows steps library import

--- backend/app/models/workflows/steps_library.py
+++ backend/app/models/workflows/steps_library.py
@@ -1,5 +1,5 @@
-from write_poem.divide_in_stanza import StanzaDividerStep
-from write_poem.write_stanza import StanzaWriterStep
+from models.workflows.write_poem.divide_in_stanza import StanzaDividerStep
+from models.workflows.write_poem.write_stanza import StanzaWriterStep
 
 steps_class = {
     "stanza_divider": StanzaDividerStep,

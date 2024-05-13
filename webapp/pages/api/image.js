682f9d41c09db2c083c9fb390dc0c0299832856b
KellyRoussel <kelly.roussel@hoomano.com>
2024-05-13 17:46:44 +0200

    image service

---
+++ webapp/pages/api/image.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+    const  apiName  = 'image';
+
+    let headers = {
+        "Content-type": req.headers["content-type"] || "application/json",
+        Authorization: req.headers.token,
+    };
+    console.log(headers);
+
+    if (req.method === "GET") {
+        try {
+            const response = await axios.get(
+                `${process.env.MOJODEX_BACKEND_URI}/${apiName}`,
+                { headers, params: req.query, responseType: 'stream' }
+            );
+            // Set headers from Python backend response
+            Object.keys(response.headers).forEach(key => {
+                res.setHeader(key, response.headers[key]);
+            });
+
+            // Stream the response from Python backend to client
+            response.data.pipe(res);
+        } catch (error) {
+            console.log("🔴 ", error);
+            return res.status(400).json({ "error": error});
+        }
+    }
+
+
+
+}

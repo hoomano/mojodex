1bda4bc47a8056dd68a97456afd250daf6d9b422
KellyRoussel <kelly.roussel@hoomano.com>
2024-06-04 14:22:27 +0200

    add chat method to mpt

--- mojodex_core/llm_engine/llm.py
+++ mojodex_core/llm_engine/llm.py
@@ -60,40 +60,6 @@ class LLM(ABC):
         """
         raise NotImplementedError
 
-    def invoke_from_mpt(self, mpt, user_id: str, temperature: float, max_tokens: int, stream: bool,stream_callback, user_task_execution_pk: int,
-                        task_name_for_system: str, **kwargs):
-        """
-        Perform LLM call from an MPT object.
-        :param mpt: MPT object.
-        :param user_id: ID of the current user.
-        :param temperature: Temperature parameter for generating responses.
-        :param max_tokens: Maximum number of tokens in the generated response.
-        :param user_task_execution_pk: Primary key of the user task execution.
-        :param task_name_for_system: Name of the task for the system.
-        :param stream: Whether to stream the response.
-        :param stream_callback: Callback function for streaming the response.
-        **kwargs: Additional keyword arguments.
-
-        :return:
-        LLM responses list
-        """
-        label = ""
-        try:
-            # put a reference to the execution with the filepath of the MPT instruction
-            # label is the filename without the file extension
-            label = mpt.filepath.split('/')[-1].split('.')[0]
-            if self.model not in mpt.models:
-                self.logger.warning(f"{mpt} does not contain model: {self.model} in its dashbangs")
-
-            messages = [{"role": "user", "content": mpt.prompt}]
-            responses = self.invoke(messages, user_id, temperature, max_tokens, label=label,
-                                    stream=stream, stream_callback=stream_callback,
-                                    user_task_execution_pk=user_task_execution_pk,
-                                    task_name_for_system=task_name_for_system, **kwargs)
-            return responses
-        except Exception as e:
-            raise Exception(f"LLM {self.name} - invoke_from_mpt - label: {label} - model: {self.model}: {e}")
-
     def _write_in_dataset(self, json_data, task_name_for_system, type, label):
         try:
             # write data in dataset_dir/label/task_name_for_system.json

91ea5af4ab6562debf09e8df1add71377b45bc92
KellyRoussel <kelly.roussel@hoomano.com>
2024-06-17 10:24:20 +0200

    internationalization

--- webapp/modules/Tasks/components/TaskExecutionDetail/TaskLoader.tsx
+++ webapp/modules/Tasks/components/TaskExecutionDetail/TaskLoader.tsx
@@ -1,14 +1,16 @@
 import Loader from "components/Loader";
 import React from "react";
+import { useTranslation } from "next-i18next";
 
 const TaskLoader = () => {
+  const { t } = useTranslation('dynamic');
   return (
     <div className="w-[100%] h-[100%] flex flex-col items-center justify-center">
       <img src="/images/writing.png" className="h-8 w-8 bg-white" />
       <div className="text-h2 font-semibold text-center py-[10px]">
-        Generating an answer..
+        {t('userTaskExecution.loading.title')}
       </div>
-      <div className="text-h5 text-gray-lighter">Please give us a moment</div>
+      <div className="text-h5 text-gray-lighter">{t('userTaskExecution.loading.subtitle')}</div>
       <Loader />
     </div>
   );

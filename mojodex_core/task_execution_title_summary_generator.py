378ed4f84dcc77f5cfc723c1e627b186d97adeac
KellyRoussel <kelly.roussel@hoomano.com>
2024-06-27 14:07:56 +0200

    no need to specify task types to generate executions title

--- mojodex_core/task_execution_title_summary_generator.py
+++ mojodex_core/task_execution_title_summary_generator.py
@@ -1,7 +1,5 @@
-from mojodex_core.entities.abstract_entities.user_task_execution import UserTaskExecution
+from mojodex_core.entities.user_task_execution import UserTaskExecution
 from mojodex_core.knowledge_manager import KnowledgeManager
-from mojodex_core.entities.db_base_entities import MdUserTaskExecution
-from mojodex_core.entities.db_base_entities import MdTask, MdUserTask
 from mojodex_core.llm_engine.mpt import MPT
 from mojodex_core.db import with_db_session
 
@@ -12,18 +10,7 @@ class TaskExecutionTitleSummaryGenerator:
     @with_db_session
     def generate_title_and_summary(user_task_execution_pk, db_session):
         try:
-            type = db_session.query(MdTask.type)\
-                .join(MdUserTask, MdTask.task_pk == MdUserTask.task_fk)\
-                .join(MdUserTaskExecution, MdUserTaskExecution.user_task_fk == MdUserTask.user_task_pk)\
-                .filter(MdUserTaskExecution.user_task_execution_pk == user_task_execution_pk)\
-                .first()[0]
-            user_task_execution: UserTaskExecution = None
-            if type == "workflow":
-                from mojodex_core.entities.user_workflow_execution import UserWorkflowExecution
-                user_task_execution = db_session.query(UserWorkflowExecution).get(user_task_execution_pk)
-            else:
-                from mojodex_core.entities.instruct_user_task_execution import InstructTaskExecution
-                user_task_execution = db_session.query(InstructTaskExecution).get(user_task_execution_pk)
+            user_task_execution = db_session.query(UserTaskExecution).get(user_task_execution_pk)
             task_execution_summary = MPT("mojodex_core/instructions/task_execution_summary.mpt",
                                         mojo_knowledge=KnowledgeManager().mojodex_knowledge,
                                         global_context=user_task_execution.user.datetime_context,

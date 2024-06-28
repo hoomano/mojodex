934cf23db6cedef0395ef88cb6c292a091dd1166
KellyRoussel <kelly.roussel@hoomano.com>
2024-06-28 11:52:39 +0200

    remove useless execution_manager

--- backend/app/models/tasks/instruct_tasks/instruct_task_manager.py
+++ backend/app/models/tasks/instruct_tasks/instruct_task_manager.py
@@ -1,7 +1,6 @@
 from models.tasks.task_executor import TaskExecutor
 
 from mojodex_core.produced_text_managers.task_produced_text_manager import TaskProducedTextManager
-from models.assistant.execution_manager import ExecutionManager
 from app import placeholder_generator
 
 from mojodex_core.tag_manager import TagManager
@@ -11,11 +10,13 @@ class InstructTaskManager:
 
     def __init__(self, session_id, user_id):
         self.task_input_manager = TagManager("ask_user_primary_info")
+        self.execution_manager = TagManager("execution")
         self.task_executor = TaskExecutor(session_id, user_id)
+
 
     @property
     def task_execution_placeholder(self):
-        return ExecutionManager.tag_manager.add_tags_to_text(
+        return self.execution_manager.add_tags_to_text(
                f"{TaskProducedTextManager.title_tag_manager.add_tags_to_text(placeholder_generator.mojo_draft_title)}"
                f"{TaskProducedTextManager.draft_tag_manager.add_tags_to_text(placeholder_generator.mojo_draft_body)}")
 
@@ -40,9 +41,9 @@ class InstructTaskManager:
             if text and mojo_message_token_stream_callback:
                 mojo_message_token_stream_callback(text)
 
-            elif ExecutionManager.tag_manager.start_tag in partial_text:
+            elif self.execution_manager.start_tag in partial_text:
                 # take the text between <execution> and </execution>
-                text = ExecutionManager.tag_manager.extract_text(partial_text)
+                text = self.execution_manager.extract_text(partial_text)
                 draft_token_stream_callback(text)
         except Exception as e:
             raise Exception(f"{self.__class__.__name__} :: manage_task_stream :: {e}")
--- backend/app/models/tasks/workflows/workflow_manager.py
+++ backend/app/models/tasks/workflows/workflow_manager.py
@@ -1,8 +1,6 @@
 from mojodex_core.tag_manager import TagManager
 from app import server_socket, placeholder_generator
 from models.workflows.workflow_process_controller import WorkflowProcessController
-
-from models.assistant.execution_manager import ExecutionManager
 from mojodex_core.produced_text_managers.task_produced_text_manager import TaskProducedTextManager
 from models.tasks.task_executor import TaskExecutor
 from mojodex_core.entities.user_workflow_execution import UserWorkflowExecution
@@ -14,6 +12,7 @@ class WorkflowManager:
         self.workflow_step_no_go_explanation_manager = TagManager("no_go_explanation")
         self.workflow_step_clarification_manager = TagManager("ask_for_clarification")
         self.workflow_step_instruction_manager = TagManager("user_instruction")
+        self.execution_manager = TagManager("execution")
         self.task_executor = TaskExecutor(session_id, user_id)
 
     @property
@@ -30,7 +29,7 @@ class WorkflowManager:
 
     @property
     def task_execution_placeholder(self):
-        return ExecutionManager.tag_manager.add_tags_to_text(
+        return self.execution_manager.add_tags_to_text(
                f"{TaskProducedTextManager.title_tag_manager.add_tags_to_text(placeholder_generator.mojo_draft_title)}"
                f"{TaskProducedTextManager.draft_tag_manager.add_tags_to_text(placeholder_generator.mojo_draft_body)}")
     def manage_response_task_tags(self, response: str, workflow_execution: UserWorkflowExecution):
@@ -63,9 +62,8 @@ class WorkflowManager:
             if text and mojo_message_token_stream_callback:
                 mojo_message_token_stream_callback(text)
 
-            elif ExecutionManager.tag_manager.start_tag in partial_text:
-                # take the text between <execution> and </execution>
-                text = ExecutionManager.tag_manager.extract_text(partial_text)
+            elif self.execution_manager.start_tag in partial_text:
+                text = self.execution_manager.extract_text(partial_text)
                 draft_token_stream_callback(text)
 
         except Exception as e:

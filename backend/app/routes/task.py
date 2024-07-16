1b889d0979bff7c2f5d8407379c49f5dfe8e2ccb
KellyRoussel <kelly.roussel@hoomano.com>
2024-07-16 11:57:23 +0200

    add additionalProperties in json task

--- backend/app/routes/task.py
+++ backend/app/routes/task.py
@@ -174,9 +174,8 @@ class Task(Resource):
                         db.session.flush()
 
 
-            #db.session.commit()
-            db.session.rollback()
-            return {"task_pk": "task.task_pk"}, 200
+            db.session.commit()
+            return {"task_pk": task.task_pk}, 200
         except Exception as e:
             log_error(f"Error creating task : {e}")
             db.session.rollback()

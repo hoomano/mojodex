d7d9fd868e60dfc162b5f3a5a1f0d0998aa5134d
xbasset <xavier.basset@hoomano.com>
2024-07-17 17:46:55 +0200

    chore: Refactor CLI menu to use MenuItem class and add new options

--- cli/main.py
+++ cli/main.py
@@ -1,5 +1,5 @@
 from rich.console import Console
-from views.menu import Menu
+from views.menu import Menu, MenuItem
 from services.auth import login
 
 def main():
@@ -7,10 +7,16 @@ def main():
     # user login
     user = login()
 
-    menu_items = ["List Tasks", "New Task"]
+    menu_items = [
+        MenuItem("New Task", lambda: print("New Task")),
+        MenuItem("List Tasks", lambda: print("List Tasks")),
+        MenuItem("Logout", lambda: print("Logout"))
+    ]
     main_menu = Menu(menu_items, intro_sentence=f"How can I help, {user.name}?")
 
-    main_menu.display(console)
+    next_action = main_menu.run(console)
+
+    next_action()
 
 
 if __name__ == "__main__":
